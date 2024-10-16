0cb586060ac6634381293efae210b13eb238b3a8
sql-trade <97407735+sql-trade@users.noreply.github.com>
2024-10-16 11:46:44 +0200

    Add files via upload

---
+++ ps__list_Installed_Software.ps1
@@ -0,0 +1,28 @@
+## ----
+## https://www.codetwo.com/admins-blog/how-to-check-installed-software-version/
+
+
+
+$list=@()
+$InstalledSoftwareKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall"
+$InstalledSoftware=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$pcname)
+$RegistryKey=$InstalledSoftware.OpenSubKey($InstalledSoftwareKey) 
+$SubKeys=$RegistryKey.GetSubKeyNames()
+
+
+Foreach ($key in $SubKeys){
+$thisKey=$InstalledSoftwareKey+"\\"+$key
+$thisSubKey=$InstalledSoftware.OpenSubKey($thisKey)
+
+$obj = New-Object PSObject
+$obj | Add-Member -MemberType NoteProperty -Name "ComputerName"   -Value $pcname
+$obj | Add-Member -MemberType NoteProperty -Name "DisplayName"    -Value $($thisSubKey.GetValue("DisplayName"))
+$obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($thisSubKey.GetValue("DisplayVersion"))
+
+$list += $obj
+}
+
+## --
+$list | where { $_.DisplayName } | select ComputerName, DisplayName, DisplayVersion | FT
+
+## ---- end ----
