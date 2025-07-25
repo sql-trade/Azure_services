1dd40f3f81f81309864164f1fe8f2584246f0401
sql-trade <97407735+sql-trade@users.noreply.github.com>
2025-07-25 13:28:43 +0200

    Update ps__find_String_in_Folder.ps1

--- ps__find_String_in_Folder.ps1
+++ ps__find_String_in_Folder.ps1
@@ -4,6 +4,7 @@
 
 ## input
 $folder = 'C:\Projects\ATAS_original_from_github\Indicators-Develop\Technical'
+$filter = "*.cs"
 $string = "UseAlert"
 $outfile = "C:\temp\_result.txt"
 
@@ -14,7 +15,7 @@ $result = $result     + "------"                            + [Environment]::New
 $result = $result     + " "
 
 $folderArray = @()
-Get-ChildItem $folder -Filter *.* -Recurse |
+Get-ChildItem $folder -Filter $filter -Recurse |
 ForEach-Object {
       If (Get-Content $_.FullName | Select-String -Pattern $string ){
           $folderArray = $folderArray + $_.FullName + [Environment]::NewLine
