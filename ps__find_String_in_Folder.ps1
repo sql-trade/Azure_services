4ba72a29619b39017306d7f4ebaf7883002ba324
sql-trade <97407735+sql-trade@users.noreply.github.com>
2025-07-25 13:23:55 +0200

    Add files via upload

---
+++ ps__find_String_in_Folder.ps1
@@ -0,0 +1,28 @@
+
+## find String in Folder ...
+
+
+## input
+$folder = 'C:\Projects\ATAS_original_from_github\Indicators-Develop\Technical'
+$string = "UseAlert"
+$outfile = "C:\temp\_result.txt"
+
+
+## ----------
+$result = "string: '" + $string + "' in folder: " + $folder + [Environment]::NewLine
+$result = $result     + "------"                      + [Environment]::NewLine
+$result = $result     + " "
+
+$folderArray = @()
+Get-ChildItem $folder -Filter *.cs -Recurse |
+ForEach-Object {
+      If (Get-Content $_.FullName | Select-String -Pattern $string ){
+          $folderArray = $folderArray + $_.FullName + [Environment]::NewLine
+      }
+}
+
+##-----------
+$result = $result + $folderArray | Sort-object | Get-Unique   ##----
+$result | Out-File $outfile
+
+## ----  end  ----
