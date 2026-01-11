256112c3b9e69a1cfe230441f54f1e997ef30ba5
sql-fabrik <git4schulung@sql-fabrik.de>
2026-01-11 10:32:47 +0100

    'ps__Azure__get_ServerDetails'  init added

---
+++ ps__Azure__get_ServerDetails.ps1
@@ -0,0 +1,49 @@
+
+CLS
+Write-Host ".. get ServerDetails"
+##--------------------------------------------------------
+
+Write-Host 
+Write-Host "MachineName: " ([environment]::MachineName)   
+Write-Host "DomainName:  " ([environment]::UserDomainName)
+Write-Host "UserName:    " ([environment]::UserName)      
+
+Write-Host 
+Write-Host "show Windows-Version:"                        
+(Get-WmiObject -class Win32_OperatingSystem).Caption      
+
+[environment]::OSVersion.VersionString                    
+##[environment]::OSVersion.Version                        
+
+
+##--------------------------------------------------------
+
+Write-Host 
+Write-Host "installed .NET version: "
+
+$release = Get-ItemPropertyValue -LiteralPath 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Release
+switch ($release) {
+    { $_ -ge 533320 } { $version = '4.8.1 or later' ; break }
+    { $_ -ge 528040 } { $version = '4.8'   ; break }
+    { $_ -ge 461808 } { $version = '4.7.2' ; break }
+    { $_ -ge 461308 } { $version = '4.7.1' ; break }
+    { $_ -ge 460798 } { $version = '4.7'   ; break }
+    { $_ -ge 394802 } { $version = '4.6.2' ; break }
+    { $_ -ge 394254 } { $version = '4.6.1' ; break }
+    { $_ -ge 393295 } { $version = '4.6'   ; break }
+    { $_ -ge 379893 } { $version = '4.5.2' ; break }
+    { $_ -ge 378675 } { $version = '4.5.1' ; break }
+    { $_ -ge 378389 } { $version = '4.5'   ; break }
+    default { $version = $null; break }
+}
+
+if ($version) {
+    Write-Host -Object ".NET Framework Version: $version"
+} else {
+    Write-Host -Object '.NET Framework Version 4.5 or later is not detected.'
+}
+Write-Host 
+
+## *********************
+## Get-ChildItem Env:   |  sort-Object Name 
+## 
